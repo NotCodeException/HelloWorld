42b272a506be14ca0527c84225ab34b7706b49d8
Administrator <469002789@qq.com>
2020-05-01 20:51:41 +0800

    first commit

--- src/com/zhang/java/HelloWorld.java
+++ src/com/zhang/java/HelloWorld.java
@@ -1,9 +1,9 @@
-package com.zhang.java;
+﻿package com.zhang.java;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("吃了吗");
+        System.out.println("吃了吗111");
 
     }
 }
